0429d966330713c3c4436d754b22f0df98174c6e
Herlan <herlanherbas@gmail.com>
2024-08-16 21:33:28 -0400

    chore: Refactor Login component to use Form component for better code organization and reusability

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import "./styles/login.css"
 import { useAuthStore } from "../../store/useAuthStore";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../config/firebaseConfig";
+import { Form } from "../../components/form/form";
 
 export default function Login() {
   const [email, setEmail] = useState<string>("");
@@ -12,42 +12,26 @@ export default function Login() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       login(userCredential.user);
       console.log("User logged in successfully");
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
-    <form>
-      <h1>Login</h1>
-      <div className="form-group">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="d-flex justify-content-center">
-        <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
-          Submit
-        </button>
-      </div>
-    </form>
+    <Form
+      title="Login"
+      email={email}
+      password={password}
+      onChangeEmail={(e) => setEmail(e.target.value)}
+      handleSubmit={(e) => handleSubmit(e)}
+      onChangePassword={(e) => setPassword(e.target.value)}
+    />
   );
 }
