c42a6a587453bd489a330bc99bd552ca0c25830e
Herlan <herlanherbas@gmail.com>
2024-08-16 23:35:04 -0400

    refactor: Refactor useTaskStore to improve code organization and readability

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -2,15 +2,21 @@ import { create } from "zustand";
 import { Task } from "../interfaces/task";
 
 interface TaskState {
-    tasks: Task[];
-    addTask: (task: Task) => void;
-    removeTask: (index: number) => void;
-    updateTask: (task: Task) => void;
+  tasks: Task[];
+  addTask: (task: Task) => void;
+  removeTask: (index: number) => void;
+  updateTask: (task: Task) => void;
 }
 
 export const useTaskStore = create<TaskState>((set) => ({
-    tasks: [],
-    addTask: (task) => set((state) => ({tasks: [...state.tasks, task]})),
-    removeTask: (index) => set((state) => ({tasks: state.tasks.filter((task) => task.index !== index)})),
-    updateTask: (task) => set((state) => ({tasks: state.tasks.map((t) => task.index === t.index ? task: t)}))
-}))
+  tasks: [],
+  addTask: (task) => set((state) => ({ tasks: [...state.tasks, task] })),
+  removeTask: (index) =>
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.index !== index),
+    })),
+  updateTask: (task) =>
+    set((state) => ({
+      tasks: state.tasks.map((t) => (task.index === t.index ? task : t)),
+    })),
+}));
