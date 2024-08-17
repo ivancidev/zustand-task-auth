5e456461c7a111b3061cee5b7d82692b21971764
Herlan <herlanherbas@gmail.com>
2024-08-17 19:19:51 -0400

    refactor: Update navbar logo link styling

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand-container">
-        <a className="navbar-brand text-white">Logo</a>
+        <a className="navbar-brand">Logo</a>
       </div>
       <div className="navbar-actions">
         <button className="btn btn-danger my-2" type="submit" onClick={logout}>
