2a320b9ad4ee46112fc0f0ebc2b6cc463641f826
Herlan <herlanherbas@gmail.com>
2024-08-16 21:33:03 -0400

    feat: Add register component for user registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Login from "./pages/Login/Login"
 import "./App.css"
+import Register from "./pages/Register/register"
 
 
 function App() {
   
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </>
   )
 }
