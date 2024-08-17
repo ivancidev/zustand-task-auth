b9a0b55bd5d538b161d5024433b4809154d21f77
Herlan <herlanherbas@gmail.com>
2024-08-17 16:51:34 -0400

    feat: Add logout functionality to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,14 @@
+import { useAuthStore } from "../../store/useAuthStore";
 import "./styles/navbar.css";
 export default function Navbar() {
+  const logout = useAuthStore((state) => state.logout);
   return (
     <nav className="navbar">
       <div className="navbar-brand-container">
         <a className="navbar-brand text-white">Logo</a>
       </div>
       <div className="navbar-actions">
-        <button className="btn btn-danger my-2" type="submit">
+        <button className="btn btn-danger my-2" type="submit" onClick={logout}>
           Logout
         </button>
       </div>
